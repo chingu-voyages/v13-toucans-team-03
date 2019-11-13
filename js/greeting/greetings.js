96d6039baa2e40d45514ff2c50334529520baa67
Zaynaib Giwa <zaynaibg@gmail.com>
2019-11-13 14:26:04 -0600

    random greet functionality is working

--- js/greeting/greetings.js
+++ js/greeting/greetings.js
@@ -1,6 +1,13 @@
-data = '[{"blue" : "is ok", "red" : "is my fave color"}]';
-
-
 let greet = document.getElementsByClassName("greeting__text")[0];
-greet.innerHTML = "Hola, My name is Ola";
-console.log(greet);
+
+let meet = $.getJSON("/v13-toucans-team-03/js/greeting/greetTypes.json", function(json) {
+    let random_num = Math.floor(Math.random()*4)
+    console.log(json); // this will show the info it in firebug console
+    console.log(json[random_num].Greet)
+    greet.innerHTML = json[random_num].Greet;
+
+});
+
+
+
+console.log(meet);
