e2130d526a204c4afb7093204d6deb92151bb1c3
Zaynaib Giwa <zaynaibg@gmail.com>
2019-11-13 14:00:30 -0600

    add JQuery to html

---
+++ js/greeting/greetings.js
@@ -0,0 +1,6 @@
+data = '[{"blue" : "is ok", "red" : "is my fave color"}]';
+
+
+let greet = document.getElementsByClassName("greeting__text")[0];
+greet.innerHTML = "Hola, My name is Ola";
+console.log(greet);
