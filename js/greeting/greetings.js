ebf98e80f9fda0297d7c73ce4c62e83e87ddc738
Zaynaib Giwa <zaynaibg@gmail.com>
2019-11-20 12:25:08 -0600

    changed greeting logic

--- js/greeting/greetings.js
+++ js/greeting/greetings.js
@@ -1,12 +1,36 @@
+//variables
 let greet = document.getElementsByClassName("greeting__text")[0];
+let greet_input = document.getElementById("greeting__input");
 
-$.getJSON("/v13-toucans-team-03/js/greeting/greetTypes.json", function(json) {
-    let random_num = Math.floor(Math.random()*4)
-    console.log(json); // this will show the info it in firebug console
-    console.log(json[random_num].Greet)
-    greet.innerHTML = json[random_num].Greet;
+//methods
 
-});
+//if local storage is not empty
+//display hello message
+
+if(localStorage.getItem('userName')){
+    greet.innerHTML = "Hello " + localStorage.getItem('userName');
+}
+
+/*
+ The hello function takes in an event. In this case the event is a keypress.
+ If the user presses the key enter( also known as the keycode 13) then setup the greeting and add the users name into localstorage.
+ 
+
+*/
+function hello(e){
+    if(e.keyCode === 13){
+        greet.innerHTML = "Hello " + greet_input.value;
+        localStorage.setItem('userName',greet_input.value);
+        greet_input.value = '';
+    }
+}
+
+
+//add event listeners
+greet_input.addEventListener('keyup',hello);
+
+
+
 
 
 
