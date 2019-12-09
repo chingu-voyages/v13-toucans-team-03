faf670f93eafae7801f655696067ac1392df83e1
RafaelDavisH <rafaeldavish443@gmail.com>
2019-12-08 22:59:22 -0800

    refactor: Update Pomodoro HTMLand JS file

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -173,17 +173,17 @@ let pomodoro = {
       this.progressHeight = 0;
     }
   };
-  const modal_pomodoro = document.getElementById("pomodoro-modal");
-  const icon_pomodoro = document.querySelectorAll(".pomodoro");
+  // const modal_pomodoro = document.getElementById("pomodoro-modal");
+  // const icon_pomodoro = document.querySelectorAll(".pomodoro");
   
-  icon_pomodoro.forEach(pdIcon => {
-    pdIcon.addEventListener("click", togglePomodoro);
-  });
+  // icon_pomodoro.forEach(pdIcon => {
+  //   pdIcon.addEventListener("click", togglePomodoro);
+  // });
   
-  function togglePomodoro() {
-    modal_pomodoro.style.visibility =
-      modal_pomodoro.style.visibility === "visible" ? "hidden" : "visible";
-  }
+  // function togglePomodoro() {
+  //   modal_pomodoro.style.visibility =
+  //     modal_pomodoro.style.visibility === "visible" ? "hidden" : "visible";
+  // }
   
   window.onload = function() {
     pomodoro.init();
