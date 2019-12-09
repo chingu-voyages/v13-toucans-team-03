6b4cfbfbf6ed20b6adc9a39ab9c3eee76ed10cbc
Zaynaib Giwa <zaynaibg@gmail.com>
2019-12-09 05:03:00 -0600

    add weather config file

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+
+app.post('/api', (request,response) => {
+    console.log(request,"node npm");
+});
+
+
